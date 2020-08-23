61fcba1028aa6d4813f2fc225b344e535fc52946
Jeremy <53671855+LuoXubo@users.noreply.github.com>
2020-08-23 15:14:39 +0800

    Create alien_invasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,42 @@
+import sys
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from game_stats import GameStats
+from button import Button
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+
+    pygame.display.set_caption("Alien Invasion")
+
+    play_button = Button(ai_settings, screen, "PLAY")
+
+    stats = GameStats(ai_settings)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    bg_color = (230,230,230)
+    alien = Alien(ai_settings, screen)
+    while True:
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens,bullets)
+        
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+       
+
+run_game()
